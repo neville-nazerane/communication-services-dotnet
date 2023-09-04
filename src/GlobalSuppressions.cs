13731ee79a423bafc51da05bd6cfe99db1e355de
Neville Nazerane <neville@mediroutes.com>
2023-09-03 22:45:44 -0700

    clean ups

---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+//[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~P:ACS.Solution.Authentication.Server.Startup.configuration")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "I don't want green squiggly everywhere", Scope = "namespaceanddescendants", Target = "~N:ACS.Solution.Authentication.Server")]
